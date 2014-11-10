c5d0ffe46823a215710214251c163ebc21382f05
Masafaggha <df.werbung@gmx.net>
2014-11-10 02:00:52 +0100

    Refactored RegularBattle.doTurn(...) and added Comparability to Turn and TurnOption

--- src/de/darthpumpkin/pkmnlib/battle/RegularBattle.java
+++ src/de/darthpumpkin/pkmnlib/battle/RegularBattle.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import de.darthpumpkin.pkmnlib.*;
+import de.darthpumpkin.pkmnlib.Pokemon.Stat;
 
 @SuppressWarnings("serial")
 public class RegularBattle extends Battle {
@@ -22,38 +23,59 @@ public class RegularBattle extends Battle {
 
 	@Override
 	public void doTurn(Map<Player, Turn> turns) {
-		// could also use Set<Turn> because Turn has attribute getParent()
-		// could also use one big comparator to sort turns, then just
-		// execute successively
-		/*
-		 * 1st: running
-		 */
-		for (Player player : players) {
-			Turn turn = turns.get(player);
-			if (turn.getOption() == TurnOption.RUN) {
+		List<Turn> turnsInOrder = new ArrayList<Turn>();
+		turnsInOrder.addAll(turns.values());
+		
+		// decide who's first
+		Collections.sort(turnsInOrder, new Comparator<Turn>() {
+			@Override
+			public int compare(Turn o1, Turn o2) {
+				int naturalTurnComparison = o1.compareTo(o2);
+				if (naturalTurnComparison != 0) {
+					return naturalTurnComparison;
+				}
+				// if we're here, both players chose the same TurnOption
+				if (o1.getOption() == Turn.TurnOption.FIGHT) {
+					// if we're here, both moves have the same priority
+					// therefore, we compare the pkmns' speed
+					return ((Float) activePokemons.get(o1.getParent())
+							.getCurrent(Stat.SPEED)).compareTo(activePokemons
+							.get(o2.getParent()).getCurrent(Stat.SPEED));
+				}
+				// no criteria left => random
+				return 0;
+			}
+		});
+		// now that we've determined order, let's execute the turns
+		for (Turn turn : turnsInOrder) {
+			Pokemon defendingPkmn = (turn.getParent() == players[0]) ? activePokemons
+					.get(players[1]) : activePokemons.get(players[0]);
+			Pokemon attackingPkmn = activePokemons.get(turn.getParent());
+			Player player = turn.getParent();
+
+			switch (turn.getOption()) {
+			case RUN:
 				if (runningEnabled) { // TODO there are moves that can prevent
-										// escaping
-					Pokemon defendingPkmn = (turn.getParent() == players[0]) ? activePokemons
-							.get(players[1]) : activePokemons.get(players[0]);
-					Pokemon attackingPkmn = activePokemons
-							.get(turn.getParent());
+					// escaping
 					if (defendingPkmn.getAbilityId() != 71
 							|| attackingPkmn.isOfType(Type.FLYING)
 							|| attackingPkmn.isFlying()) {
 						// 71 == 'arena trap'; but still works when flying-type
 						// or in the air
 						// TODO implement escape
+					} else {
+						// TODO log running is not possible because of arena
+						// trap
 					}
+				} else {
+					// TODO log running is not possible (trainer battle etc.)
 				}
-			}
-		}
-		/*
-		 * 2nd: swapping
-		 */
-		for (Player player : players) {
-			if (turns.get(player).getOption() == TurnOption.SWAP_PKMN) {
-				if (true) { // TODO there are moves and abilities that can
-							// prevent swapping
+				break;
+			case SWAP_PKMN:
+				if (defendingPkmn.getAbilityId() != 71
+						|| attackingPkmn.isOfType(Type.FLYING)
+						|| attackingPkmn.isFlying()) {
+					// TODO there are moves that can prevent swapping
 					Pokemon p = player.getTeam()[turns.get(player)
 							.getTargetId()]; // targetId is the index of the
 												// pokemon to be sent in
@@ -64,219 +86,169 @@ public class RegularBattle extends Battle {
 					withdrawPokemon(player);
 					sendPokemon(player, p);
 				}
-			}
-		}
-		/*
-		 * 3rd: item usage
-		 */
-		for (Player player : players) {
-			if (turns.get(player).getOption() == TurnOption.USE_ITEM) {
+				break;
+			case USE_ITEM:
 				Item item = turns.get(player).getItem();
 				item.getParent().remove(item);
 				// TODO implement item usage
-			}
-		}
-		/*
-		 * 4th: regular move
-		 */
-		List<Turn> turnsInOrder = new ArrayList<Turn>();
-		turnsInOrder.addAll(turns.values());
-		for (Turn turn : turnsInOrder) {
-			if (turn.getMove().getCurrentPp() <= 0) {
-				throw new RuntimeException("no PP left!");
-			}
-		}
-		// decide who's first
-		Collections.sort(turnsInOrder, new Comparator<Turn>() {
-			@Override
-			public int compare(Turn o0, Turn o1) {
-				// first, compare priotities
-				int[] prios = new int[] { o0.getMove().getPriority(),
-						o1.getMove().getPriority() };
-				if (prios[0] < prios[1]) {
-					return -1;
-				}
-				if (prios[1] < prios[0]) {
-					return 1;
-				} else { // priorities are eaual, compare speed
-					int result = 0;
-					float[] speeds = new float[] {
-							activePokemons.get(o0.getParent()).getCurrent(
-									Pokemon.Stat.SPEED),
-							activePokemons.get(o1.getParent()).getCurrent(
-									Pokemon.Stat.SPEED) };
-					if (speeds[0] < speeds[1]) {
-						result = -1;
-					}
-					if (speeds[0] < speeds[1]) {
-						result = 1;
-					}
-					if (trickRoomCounter != 0) {
-						result = -result;
-					}
-					return result;
-				}
-			}
-		});
-		for (Turn turn : turnsInOrder) {
-			Pokemon attackingPkmn = activePokemons.get(turn.getParent());
-			if (!attackingPkmn.isUsable()) {
-				// pkmn fainted before it could attack
-				continue;
-			}
-			// TODO check if attack hits (accuracy, evasion...)
-			// if we're here, the attack was successful; now apply the effects
-			switch (turn.getMove().getEffectId()) {
-			case 1: // regular damage, see
-					// http://www.smogon.com/dp/articles/damage_formula
-				Pokemon defendingPkmn = (turn.getParent() == players[0]) ? activePokemons
-						.get(players[1]) : activePokemons.get(players[0]);
-				/*
-				 * level
-				 */
-				// int division will automatically floor
-				int level = attackingPkmn.getLevel() * 2 / 5 + 2;
-
-				/*
-				 * base power
-				 */
-				int power = turn.getMove().getPower();
-				double helpingHand = 1d; // not relevant in single battle
-				// see http://www.smogon.com/dp/articles/damage_formula#bp_items
-				double itemMultiplier = 1d;
-				int charge = 1; // TODO 2 if the last move was charge and the
-								// move's type is electric
-				double sport = 1d; // TODO modificator for mud sport and water
-									// sport
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#bp_abilities_user
-				// and
-				// http://www.smogon.com/dp/articles/damage_formula#bp_abilities_foe
-				double abilityMultiplier = 1d;
-
-				// multiplying all beforementioned values with flooring after
-				// each step for the final basePower value
-				double basePower = Math
-						.floor(Math.floor(Math.floor(Math.floor(Math
-								.floor(helpingHand * power) * itemMultiplier)
-								* charge)
-								* sport)
-								* abilityMultiplier);
-				/*
-				 * [Sp]Atk
-				 */
-				double atkStat = (turn.getMove().getDamageClass() == Move.DamageClass.PHYSICAL) ? attackingPkmn
-						.getCurrent(Pokemon.Stat.ATK) : attackingPkmn
-						.getCurrent(Pokemon.Stat.SPATK);
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#atk_abilities
-				double abilityModifier = 1d;
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#atk_items
-				double itemModifier = 1d;
-
-				// multiplying all beforementioned values with flooring after
-				// each step for the final atk value
-				double atk = Math.floor(Math.floor(Math.floor(atkStat
-						* abilityModifier)
-						* itemModifier) / 50);
-
-				/*
-				 * [Sp]Def
-				 */
-				double defStat = (turn.getMove().getDamageClass() == Move.DamageClass.PHYSICAL) ? defendingPkmn
-						.getCurrent(Pokemon.Stat.DEF) : defendingPkmn
-						.getCurrent(Pokemon.Stat.SPDEF);
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#defense
-				double sx = 1d;
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#defense
-				double mod = 1d;
-
-				// multiplying all beforementioned values with flooring after
-				// each step for the final def value
-				double def = Math.floor(Math.floor(defStat * sx) * mod);
-
-				/*
-				 * MOD1
-				 */
-				// TODO brn is always 1 if the attacker's ability is 'guts'
-				double brn = (turn.getMove().getDamageClass() == Move.DamageClass.PHYSICAL && attackingPkmn
-						.getStatusProblem() == Pokemon.StatusProblem.BURN) ? 0.5
-						: 1;
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#mod1
-				double rl = 1d;
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#mod1
-				double weatherModifier = 1d;
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#mod1
-				double ff = 1d;
-
-				// multiplying all beforementioned values for the final mod1
-				// value
-				double mod1 = brn * rl * weatherModifier * ff;
-
-				/*
-				 * Critical hit
-				 */
-				double critical = 1d; // TODO critical hits
-
-				/*
-				 * MOD2
-				 */
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#mod2
-				double mod2 = 1d;
-
-				/*
-				 * R
-				 */
-				double r = 100 - (15 * Math.random()); // [85, 100]
-
-				/*
-				 * STAB
-				 */
-				// TODO stab is 2 if the attaacker's ability is adaptability
-				double stab = turn.getMove().getType().getStab(attackingPkmn);
-
-				/*
-				 * TYPE1 and TYPE2
-				 */
-				double eff = turn.getMove().getType()
-						.getEffectivenessOver(defendingPkmn);
-
-				/*
-				 * MOD3
-				 */
-				// TODO see
-				// http://www.smogon.com/dp/articles/damage_formula#mod3
-				double mod3 = 1d;
-
-				/*
-				 * now, the final formula
-				 */
-				int damage = (int) Math.round(Math.round((Math.round(Math
-						.round(Math.round(level * basePower * atk / 50) / def)
-						* mod1) + 2) * critical * mod2)
-						* r / 100);
-				defendingPkmn.applyDamage(damage);
-				// TODO log!
 				break;
-			// TODO implement effectIds
-			default:
-				throw new RuntimeException("Unknown effect id");
-			}
-		}
-		// check if there are pkmns who fainted
-		for (Player player : players) {
-			if (!activePokemons.get(player).isUsable()) {
-				if (player.forceSwitch(activePokemons.get(player)) == null) {
-					this.active = false;
-					// TODO we have a winner
+			case FIGHT:
+				if (!attackingPkmn.isUsable()) {
+					// pkmn fainted before it could attack
+					continue;
 				}
+				// TODO check if attack hits (accuracy, evasion...)
+				
+				// if we're here, the attack was successful; now apply the
+				// effects
+				switch (turn.getMove().getEffectId()) {
+				case 1: // regular damage, see
+						// http://www.smogon.com/dp/articles/damage_formula
+					/*
+					 * level
+					 */
+					// int division will automatically floor
+					int level = attackingPkmn.getLevel() * 2 / 5 + 2;
+
+					/*
+					 * base power
+					 */
+					int power = turn.getMove().getPower();
+					double helpingHand = 1d; // not relevant in single battle
+					// see
+					// http://www.smogon.com/dp/articles/damage_formula#bp_items
+					double itemMultiplier = 1d;
+					int charge = 1; // TODO 2 if the last move was charge and
+									// the
+									// move's type is electric
+					double sport = 1d; // TODO modificator for mud sport and
+										// water
+										// sport
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#bp_abilities_user
+					// and
+					// http://www.smogon.com/dp/articles/damage_formula#bp_abilities_foe
+					double abilityMultiplier = 1d;
+
+					// multiplying all beforementioned values with flooring
+					// after
+					// each step for the final basePower value
+					double basePower = Math
+							.floor(Math.floor(Math.floor(Math.floor(Math
+									.floor(helpingHand * power) * itemMultiplier)
+									* charge)
+									* sport)
+									* abilityMultiplier);
+					/*
+					 * [Sp]Atk
+					 */
+					double atkStat = (turn.getMove().getDamageClass() == Move.DamageClass.PHYSICAL) ? attackingPkmn
+							.getCurrent(Pokemon.Stat.ATK) : attackingPkmn
+							.getCurrent(Pokemon.Stat.SPATK);
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#atk_abilities
+					double abilityModifier = 1d;
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#atk_items
+					double itemModifier = 1d;
+
+					// multiplying all beforementioned values with flooring
+					// after
+					// each step for the final atk value
+					double atk = Math.floor(Math.floor(Math.floor(atkStat
+							* abilityModifier)
+							* itemModifier) / 50);
+
+					/*
+					 * [Sp]Def
+					 */
+					double defStat = (turn.getMove().getDamageClass() == Move.DamageClass.PHYSICAL) ? defendingPkmn
+							.getCurrent(Pokemon.Stat.DEF) : defendingPkmn
+							.getCurrent(Pokemon.Stat.SPDEF);
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#defense
+					double sx = 1d;
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#defense
+					double mod = 1d;
+
+					// multiplying all beforementioned values with flooring
+					// after
+					// each step for the final def value
+					double def = Math.floor(Math.floor(defStat * sx) * mod);
+
+					/*
+					 * MOD1
+					 */
+					// TODO brn is always 1 if the attacker's ability is 'guts'
+					double brn = (turn.getMove().getDamageClass() == Move.DamageClass.PHYSICAL && attackingPkmn
+							.getStatusProblem() == Pokemon.StatusProblem.BURN) ? 0.5
+							: 1;
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#mod1
+					double rl = 1d;
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#mod1
+					double weatherModifier = 1d;
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#mod1
+					double ff = 1d;
+
+					// multiplying all beforementioned values for the final mod1
+					// value
+					double mod1 = brn * rl * weatherModifier * ff;
+
+					/*
+					 * Critical hit
+					 */
+					double critical = 1d; // TODO critical hits
+
+					/*
+					 * MOD2
+					 */
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#mod2
+					double mod2 = 1d;
+
+					/*
+					 * R
+					 */
+					double r = 100 - (15 * Math.random()); // [85, 100]
+
+					/*
+					 * STAB
+					 */
+					// TODO stab is 2 if the attaacker's ability is adaptability
+					double stab = turn.getMove().getType().getStab(attackingPkmn);
+
+					/*
+					 * TYPE1 and TYPE2
+					 */
+					double eff = turn.getMove().getType()
+							.getEffectivenessOver(defendingPkmn);
+
+					/*
+					 * MOD3
+					 */
+					// TODO see
+					// http://www.smogon.com/dp/articles/damage_formula#mod3
+					double mod3 = 1d;
+
+					/*
+					 * now, the final formula
+					 */
+					int damage = (int) Math.floor(Math.floor((Math.floor(Math
+							.floor(Math.floor(level * basePower * atk / 50) / def)
+							* mod1) + 2) * critical * mod2)
+							* r / 100);
+					defendingPkmn.applyDamage(damage);
+					// TODO log!
+					break;
+				// TODO implement other effectIds
+				default:
+					throw new RuntimeException("Unknown effect id");
+				}
+				break;
 			}
 		}
 	}
--- src/de/darthpumpkin/pkmnlib/battle/Turn.java
+++ src/de/darthpumpkin/pkmnlib/battle/Turn.java
@@ -6,48 +6,80 @@ import de.darthpumpkin.pkmnlib.Item;
 import de.darthpumpkin.pkmnlib.Move;
 
 @SuppressWarnings("serial")
-public class Turn implements Serializable {
+public class Turn implements Serializable, Comparable<Turn> {
+
+	/**
+	 * RUN > SWAP_PKMN > USE_ITEM > FIGHT
+	 */
+	public enum TurnOption {
+		FIGHT, USE_ITEM, SWAP_PKMN, RUN
+	}
+
 	private TurnOption option;
 	private Move move;
 	private Item item;
 	private int targetId;
 	private Player parent;
-	
+
 	/*
 	 * getters and setters
 	 */
 	public TurnOption getOption() {
 		return option;
 	}
+
 	public void setOption(TurnOption option) {
 		this.option = option;
 	}
+
 	public Move getMove() {
 		return move;
 	}
+
 	public void setMove(Move move) {
 		this.move = move;
 	}
+
 	public Item getItem() {
 		return item;
 	}
+
 	public void setItem(Item item) {
 		this.item = item;
 	}
+
 	public int getTargetId() {
 		return targetId;
 	}
+
 	public void setTargetId(int targetId) {
 		this.targetId = targetId;
 	}
+
 	public Player getParent() {
 		return parent;
 	}
+
 	public void setParent(Player parent) {
 		this.parent = parent;
 	}
-}
 
-enum TurnOption {
-	FIGHT, SWAP_PKMN, USE_ITEM, RUN
+	/**
+	 * first compares turn options. if equal, compares move priorities. Note:
+	 * this does not respect the pkmn's speed or any other information related
+	 * to the current state of the battle as a turn does not know about the
+	 * battle.
+	 */
+	@Override
+	public int compareTo(Turn otherTurn) {
+		int optionComparison = this.option.compareTo(otherTurn.getOption());
+		if (optionComparison != 0) {
+			return optionComparison;
+		}
+		if (option == TurnOption.FIGHT) {
+			return ((Integer) this.move.getPriority()).compareTo(otherTurn
+					.getMove().getPriority());
+		}
+		return 0;
+	}
 }
