7aa15d4ffba34bcd4da7eb733a5b08f60135d1a6
Masafaggha <df.werbung@gmx.net>
2015-02-10 03:07:09 +0100

    Pokemon class will need to be split up. Proposal is provided in an ER-diagram. Also added a provisional short example usage

---
+++ doc/pkmnlib_usage.java
@@ -0,0 +1,32 @@
+/*
+ * Short example usage of pkmnlib.
+ * 
+ * Assuming you already implemented a PokemonSpeciesFactory 'PkmnFactoryImpl'
+ */
+
+//"a wild rattata appeared!"
+//1.Build a rattata instance
+PokemonFactory factory = new PkmnFactoryImpl();
+PokemonSpecies rattataSpecies = factory.getSpeciesByName("Rattata");
+PokemonInstanceBuilder rattataBuilder = new PokemonInstanceBuilder(rattataSpecies);
+rattataBuilder.setLevel(3);
+PokemonInstance rattataInstance = rattataBuilder.getInstance();
+//2.Start the battle
+List<PokemonInstance> rattataTeam = new ArrayList<PokemonInstance>();
+rattataTeam.add(rattataInstance);
+Player opponent = new StupidPlayer(rattataTeam);
+RegularBattle battle = new RegularBattle(me, opponent);
+battle.start();
+
+//Win the battle
+while(battle.isActive()) {
+	List<Turns> turns = new ArrayList<Turn>();
+	for (Player p : battle.getPlayers()) {
+		turns.add(p.makeTurn());
+	}
+	battle.doTurn(turns);
+}
+//Print results
+for (AtomarBattleAction action : battle.dumpActionLog()) {
+	System.out.println(action.toString());
+}
