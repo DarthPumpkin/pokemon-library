7a33a9a5c9be49cb47d421590ac30e76a660780d
Masafaggha <df.werbung@gmx.net>
2015-02-15 20:30:39 +0100

    Added test suite

---
+++ test/de/darthpumpkin/pkmnlib/test/AllTests.java
@@ -0,0 +1,11 @@
+package de.darthpumpkin.pkmnlib.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PokemonSpeciesTest.class, PokemonInstanceBuilderTest.class})
+public class AllTests {
+
+}
