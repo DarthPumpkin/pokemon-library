b7dc9638657f628dd8ccc37dfa1b8c05605eae07
DarthPumpkin <DarthPumpkin@users.noreply.github.com>
2015-02-26 22:07:42 +0100

    Move class was given its structure

--- src/de/darthpumpkin/pkmnlib/Move.java
+++ src/de/darthpumpkin/pkmnlib/Move.java
@@ -1,6 +1,7 @@
 package de.darthpumpkin.pkmnlib;
 
 import java.io.Serializable;
+import java.util.EnumSet;
 
 /**
  * This move model is designed after veekun's db, especially the table 'moves'
@@ -15,49 +16,92 @@ import java.io.Serializable;
  */
 @SuppressWarnings("serial")
 public class Move implements Serializable {
-	
+
 	public enum DamageClass {
-		STATUS, PHYSICAL, SPECIAL
+		PHYSICAL, SPECIAL, STATUS
 	}
 
-	private int id;
-	private String name;
+	public enum MoveFlag {
+		authentic, ballistics, bite, charge, contact, defrost, distance, gravity, heal, mental, mirror, nonSkyBattle, powder, protect, pulse, punch, recharge, reflectable, snatch, sound
+	}
+
+	private int accuracy;
+	private int basePp;
+	private int currentPp;
 	private DamageClass damageClass;
+	private int effectChance;
+	private int effectId;
+	private EnumSet<MoveFlag> flags;
+	private int id;
+	private int maximumPp; // equals basePp if not raised by Ap-Plus etc.
+	private int power;
+	private int priority;
+	// private int target;
 	private Type type;
 
-	private int effectId;
-	private int effectChance;
-
-	private int power;
-	private int currentPp;
-	private int maximumPp;	//equals basePp if not raised by Ap-Plus etc.
-	private int basePp;
-	private int accuracy;
-	private int priority;
-	private int target;
-	
 	/**
 	 * Provisional constructor
+	 * 
 	 * @param id
 	 */
 	public Move(int id) {
 		this.setId(id);
 	}
 
-	public int getPriority() {
-		return priority;
+	public int getAccuracy() {
+		return accuracy;
 	}
 
-	public int getEffectId() {
-		return effectId;
+	public void setAccuracy(int accuracy) {
+		this.accuracy = accuracy;
+	}
+
+	public int getBasePp() {
+		return basePp;
+	}
+
+	public void setBasePp(int basePp) {
+		this.basePp = basePp;
 	}
 
 	public int getCurrentPp() {
 		return currentPp;
 	}
 
+	public void setCurrentPp(int currentPp) {
+		this.currentPp = currentPp;
+	}
+
 	public DamageClass getDamageClass() {
-		return this.damageClass;
+		return damageClass;
+	}
+
+	public void setDamageClass(DamageClass damageClass) {
+		this.damageClass = damageClass;
+	}
+
+	public int getEffectChance() {
+		return effectChance;
+	}
+
+	public void setEffectChance(int effectChance) {
+		this.effectChance = effectChance;
+	}
+
+	public int getEffectId() {
+		return effectId;
+	}
+
+	public void setEffectId(int effectId) {
+		this.effectId = effectId;
+	}
+
+	public EnumSet<MoveFlag> getFlags() {
+		return flags;
+	}
+
+	public void setFlags(EnumSet<MoveFlag> flags) {
+		this.flags = flags;
 	}
 
 	public int getId() {
@@ -68,12 +112,28 @@ public class Move implements Serializable {
 		this.id = id;
 	}
 
+	public int getMaximumPp() {
+		return maximumPp;
+	}
+
+	public void setMaximumPp(int maximumPp) {
+		this.maximumPp = maximumPp;
+	}
+
 	public int getPower() {
 		return power;
 	}
 
 	public void setPower(int power) {
 		this.power = power;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
 	}
 
 	public Type getType() {
@@ -83,4 +143,5 @@ public class Move implements Serializable {
 	public void setType(Type type) {
 		this.type = type;
 	}
+
 }
