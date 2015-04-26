29a79c2e8c64f74e6980f7f8f777b7b44845b66c
DarthPumpkin <DarthPumpkin@users.noreply.github.com>
2015-04-27 01:20:12 +0200

    Implement dummy move tree provider
    - Add simple XML-based move tree provider class
    - Test cases for dummy provider
    - Minor fixes in XML

---
+++ src/de/darthpumpkin/pkmnlib/DummyXMLMoveTreeProvider.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package de.darthpumpkin.pkmnlib;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * @author dominik
+ * 
+ */
+public class DummyXMLMoveTreeProvider implements MoveTreeProvider {
+
+	private static final String FILE_PREFIX;
+	private static final String FILE_POSTFIX;
+	private static final String PROBABILITY_TAG;
+	private static final String EFFECT_ID_TAG;
+	private static final String SUBELEMENT_TAG;
+	private static final String OCCASION_ATTRIBUTE_NAME;
+	private static final String SUCCESS_VALUE;
+	private static final String FAILURE_VALUE;
+
+	static {
+		FILE_PREFIX = "data/";
+		FILE_POSTFIX = ".xml";
+		PROBABILITY_TAG = "probability";
+		EFFECT_ID_TAG = "effectId";
+		SUBELEMENT_TAG = "atomar";
+		OCCASION_ATTRIBUTE_NAME = "occasion";
+		SUCCESS_VALUE = "success";
+		FAILURE_VALUE = "failure";
+	}
+
+	private Move move;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.darthpumpkin.pkmnlib.MoveTreeProvider#setMove(de.darthpumpkin.pkmnlib
+	 * .Move)
+	 */
+	@Override
+	public void setMove(Move move) {
+		this.move = move;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.darthpumpkin.pkmnlib.MoveTreeProvider#attachMoveTree()
+	 */
+	@Override
+	public void attachMoveTree() {
+		int moveId = move.getId();
+		String filename = FILE_PREFIX + moveId + FILE_POSTFIX;
+		try {
+			Document doc = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder().parse(filename);
+			doc.getDocumentElement().normalize();
+			NodeList roots = doc.getElementsByTagName(SUBELEMENT_TAG);
+			for (int i = 0; i < roots.getLength(); i++) {
+				Element root = (Element) roots.item(i);
+				AtomarMove rootAtomarMove = makeAtomarMove(root);
+				String occasion = root.getAttribute(OCCASION_ATTRIBUTE_NAME);
+				if (occasion.equals(SUCCESS_VALUE)) {
+					move.setSuccessElement(rootAtomarMove);
+				} else if (occasion.equals(FAILURE_VALUE)) {
+					move.setFailureElement(rootAtomarMove);
+				} else {
+					throw new RuntimeException("Malformed XML: " + occasion
+							+ " is not a valid occasion");
+				}
+			}
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private AtomarMove makeAtomarMove(Element doc) {
+		NodeList probability = doc.getElementsByTagName(PROBABILITY_TAG);
+		NodeList effectId = doc.getElementsByTagName(EFFECT_ID_TAG);
+		NodeList subelements = doc.getElementsByTagName(SUBELEMENT_TAG);
+		/*
+		 * Exception handling
+		 */
+		if (probability.getLength() != 1) {
+			throw new RuntimeException("Malformed XML: "
+					+ probability.getLength() + " probabilities");
+		}
+		if (effectId.getLength() != 1) {
+			throw new RuntimeException("Malformed XML: " + effectId.getLength()
+					+ " effectIds");
+		}
+		if (subelements.getLength() > 2) {
+			throw new RuntimeException("Malformed XML: "
+					+ subelements.getLength() + " atomars");
+		}
+		AtomarMove am = new AtomarMove();
+		System.out.println(effectId);
+		am.setEffectId(Integer.parseInt(effectId.item(0).getTextContent()));
+		am.setProbability(Integer
+				.parseInt(probability.item(0).getTextContent()));
+		for (int i = 0; i < subelements.getLength(); i++) {
+			Element subelement = (Element) subelements.item(i);
+			String occasion = subelement.getAttribute(OCCASION_ATTRIBUTE_NAME);
+			if (occasion.equals(SUCCESS_VALUE)) {
+				am.setSuccessElement(makeAtomarMove(subelement));
+			} else if (occasion.equals(FAILURE_VALUE)) {
+				am.setFailureElement(makeAtomarMove(subelement));
+			} else {
+				throw new RuntimeException("Malformed XML: " + occasion
+						+ " is not a valid occasion");
+			}
+		}
+		return am;
+	}
+
+}
---
+++ test/de/darthpumpkin/pkmnlib/test/DummyXMLProviderTest.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package de.darthpumpkin.pkmnlib.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.darthpumpkin.pkmnlib.AtomarMove;
+import de.darthpumpkin.pkmnlib.DummyXMLMoveTreeProvider;
+import de.darthpumpkin.pkmnlib.Move;
+
+/**
+ * @author dominik
+ * 
+ */
+public class DummyXMLProviderTest {
+
+	private DummyXMLMoveTreeProvider provider;
+
+	@Before
+	public void setUpBefore() {
+		provider = new DummyXMLMoveTreeProvider();
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.darthpumpkin.pkmnlib.DummyXMLMoveTreeProvider#attachMoveTree()}
+	 * with Move 'Tackle'.
+	 */
+	@Test
+	public void testAttachMoveTreeTackle() {
+		Move tackle = new Move(33);
+		provider.setMove(tackle);
+		provider.attachMoveTree();
+		assertEquals(tackle.getId(), 33);
+		AtomarMove succ = tackle.getSuccessElement();
+		assertNotNull(succ);
+		assertNull(tackle.getFailureElement());
+		assertEquals(succ.getEffectId(), 1);
+		assertEquals(succ.getProbability(), 100);
+		assertNull(succ.getSuccessElement());
+		assertNull(succ.getFailureElement());
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.darthpumpkin.pkmnlib.DummyXMLMoveTreeProvider#attachMoveTree()}
+	 * with Move 'Growl'.
+	 */
+	@Test
+	public void testAttachMoveTreeGrowl() {
+		Move growl = new Move(45);
+		provider.setMove(growl);
+		provider.attachMoveTree();
+		assertEquals(growl.getId(), 45);
+		AtomarMove succ = growl.getSuccessElement();
+		assertNotNull(succ);
+		assertNull(growl.getFailureElement());
+		assertEquals(succ.getEffectId(), 101);
+		assertEquals(succ.getProbability(), 100);
+		assertNull(succ.getSuccessElement());
+		assertNull(succ.getFailureElement());
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.darthpumpkin.pkmnlib.DummyXMLMoveTreeProvider#attachMoveTree()}
+	 * with Move 'Leech-Seed'.
+	 */
+	@Test
+	public void testAttachMoveTreeLeechSeed() {
+		Move leechSeed = new Move(73);
+		provider.setMove(leechSeed);
+		provider.attachMoveTree();
+		assertEquals(leechSeed.getId(), 73);
+		AtomarMove succ = leechSeed.getSuccessElement();
+		assertNotNull(succ);
+		assertNull(leechSeed.getFailureElement());
+		assertEquals(succ.getEffectId(), 1001);
+		assertEquals(succ.getProbability(), 100);
+		assertNull(succ.getSuccessElement());
+		assertNull(succ.getFailureElement());
+	}
+
+}
