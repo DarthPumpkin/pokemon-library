75664b4744dbc89383a1192b79ea2104208a66ea
DarthPumpkin <DarthPumpkin@users.noreply.github.com>
2015-04-27 21:12:40 +0200

    Move Weather to separate file

--- src/de/darthpumpkin/pkmnlib/battle/AbstractBattle.java
+++ src/de/darthpumpkin/pkmnlib/battle/AbstractBattle.java
@@ -7,46 +7,52 @@ import java.util.Map;
 
 /**
  * Superclass for all battle implementations
+ * 
  * @author dominik
- *
+ * 
  */
 public abstract class AbstractBattle implements Serializable {
-	
+
 	protected final Player[] players;
 	protected Weather weather;
-	protected int trickRoomCounter;	//remaining turns where trick room is active
+	protected int trickRoomCounter; // remaining turns where trick room is
+									// active
 	protected List<AtomarBattleAction> actionLog;
-	
+
 	protected void log(AtomarBattleAction action) {
 		actionLog.add(action);
 	}
-	
+
 	public AbstractBattle(Player[] players, Weather weather) {
 		this.players = players;
 		this.weather = weather;
 		this.trickRoomCounter = 0;
 		this.actionLog = new ArrayList<AtomarBattleAction>();
 	}
-	
+
 	/**
 	 * start the battle.
 	 */
 	public abstract void start();
-	
+
 	/**
 	 * executes the turns submitted by the players and applies the results
-	 * @param turns the submitted turns mapped to their respective player
+	 * 
+	 * @param turns
+	 *            the submitted turns mapped to their respective player
 	 */
 	public abstract void doTurn(Map<Player, Turn> turns);
-	
+
 	/**
 	 * determines whether the battle is over
+	 * 
 	 * @return false if the battle is over, true if not
 	 */
 	public abstract boolean isActive();
-	
+
 	/**
 	 * clear and return the actionLog
+	 * 
 	 * @return
 	 */
 	public List<AtomarBattleAction> dumpActionLog() {
@@ -54,14 +60,12 @@ public abstract class AbstractBattle implements Serializable {
 		this.actionLog = new ArrayList<AtomarBattleAction>();
 		return latestLog;
 	}
+
 	public Player[] getPlayers() {
 		return players;
 	}
+
 	public Weather getWeather() {
 		return weather;
 	}
-}
-
-enum Weather {
-	NORMAL, SUN, RAIN, HAIL, SAND
 }
---
+++ src/de/darthpumpkin/pkmnlib/battle/Weather.java
@@ -0,0 +1,5 @@
+package de.darthpumpkin.pkmnlib.battle;
+
+public enum Weather {
+	NORMAL, SUN, RAIN, HAIL, SAND
+}
