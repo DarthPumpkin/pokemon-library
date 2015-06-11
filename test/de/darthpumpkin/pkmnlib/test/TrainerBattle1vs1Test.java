b1f7bf558acd32831d46b4a662619bc8980393f1
DarthPumpkin <DarthPumpkin@users.noreply.github.com>
2015-06-11 22:32:39 +0200

    Add test for TrainerBattle1vs1

--- test/de/darthpumpkin/pkmnlib/test/TrainerBattle1vs1Test.java
+++ test/de/darthpumpkin/pkmnlib/test/TrainerBattle1vs1Test.java
@@ -4,6 +4,7 @@
 package de.darthpumpkin.pkmnlib.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
@@ -96,7 +97,7 @@ public class TrainerBattle1vs1Test {
 	 */
 	@Test
 	public void testDoTurn1() {
-		TrainerBattle1vs1 battle = makeEasyBulbaBattle();
+		TrainerBattle1vs1 battle = makeEasyBulbaBattle(1);
 		battle.start();
 		SingleBattlePlayer[] players = (SingleBattlePlayer[]) battle
 				.getPlayers();
@@ -109,14 +110,59 @@ public class TrainerBattle1vs1Test {
 			assertEquals(8, pbi.getInstance().getCurrentHp());
 		}
 	}
-	
+
+	/**
+	 * Test method for
+	 * {@link de.darthpumpkin.pkmnlib.battle.TrainerBattle1vs1#doTurn()}. 2nd
+	 * case: Two lvl 12 bulbasaurs using tackle at full health (lvl 12 is the
+	 * last one before forgetting tackle)
+	 */
+	@Test
+	public void testDoTurn2() {
+		TrainerBattle1vs1 battle = makeEasyBulbaBattle(10);
+		battle.start();
+		SingleBattlePlayer[] players = (SingleBattlePlayer[]) battle
+				.getPlayers();
+		HashMap<Player, Turn> turns = new HashMap<Player, Turn>();
+		for (Player p : players) {
+			turns.put(p, p.makeTurn());
+		}
+		battle.doTurn(turns);
+		for (PokemonBattleInstance pbi : battle.getActivePkmns()) {
+			assertEquals(22.0, pbi.getInstance().getCurrentHp(), 1.001);
+		}
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.darthpumpkin.pkmnlib.battle.TrainerBattle1vs1#doTurn()}. 
+	 * 3rd case: first bulbasaur faints
+	 */
+	@Test
+	public void testDoTurnFaint() {
+		TrainerBattle1vs1 battle = makeEasyBulbaBattle(1);
+		battle.start();
+		SingleBattlePlayer[] players = (SingleBattlePlayer[]) battle.getPlayers();
+		PokemonInstance weakPkmn = players[0].getActivePokemon().getInstance(); 
+		weakPkmn.applyDamage(10);
+		assertEquals(1, players[0].getTeam()[0].getCurrentHp());
+		HashMap<Player, Turn> turns = new HashMap<Player, Turn>();
+		for (Player p : players) {
+			turns.put(p, p.makeTurn());
+		}
+		battle.doTurn(turns);
+		assertEquals(0, weakPkmn.getCurrentHp());
+		assertFalse(weakPkmn.isUsable());
+		assertFalse(battle.isActive());
+	}
+
 	// TODO more test cases for doTurn()
 
-	private TrainerBattle1vs1 makeEasyBulbaBattle() {
+	private TrainerBattle1vs1 makeEasyBulbaBattle(int level) {
 		PokemonInstance[] team1 = new PokemonInstance[] { new PokemonInstanceBuilder(
-				bulbaSpecies).setLevel(1).makePokemon() };
+				bulbaSpecies).setLevel(level).makePokemon() };
 		PokemonInstance[] team2 = new PokemonInstance[] { new PokemonInstanceBuilder(
-				bulbaSpecies).setLevel(1).makePokemon() };
+				bulbaSpecies).setLevel(level).makePokemon() };
 		SingleBattlePlayer[] players = new DummyPlayer[] {
 				new DummyPlayer(team1), new DummyPlayer(team2) };
 		return new TrainerBattle1vs1(players, Weather.NORMAL);
